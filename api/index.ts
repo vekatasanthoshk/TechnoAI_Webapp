545e893594ccee8aa7b8ac70b180e677fbc4131f
Venkata Santhosh K <venkatasanthoshk@gmail.com>
2026-06-16 03:43:48 -0500

    fix: remove registerOAuthRoutes from api/index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,6 @@
 import "dotenv/config";
 import express from "express";
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
-import { registerOAuthRoutes } from "../server/_core/oauth";
 import { appRouter } from "../server/routers";
 import { createContext } from "../server/_core/context";
 
@@ -9,8 +8,6 @@ const app = express();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-
-registerOAuthRoutes(app);
 
 app.use(
   "/api/trpc",
